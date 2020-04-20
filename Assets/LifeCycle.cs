14f14d84df233e51831b443304587865553a3391
하늘 정 <jhn121@naver.com>
2020-04-20 21:55:48 +0900

    staging test

---
+++ Assets/LifeCycle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycle : MonoBehaviour
+{
+   void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            Debug.Log("플레이어가 아무키를 눌렀습니다.");
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("아이템을 구입하였습니다.");
+                    }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("왼쪽으로 이동중");
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+            Debug.Log("오른쪽으로 이동중");
+            }
+        if(Input.GetKey(KeyCode.UpArrow))
+            {
+            Debug.Log("위로 이동중");
+            }
+    }
+}
